c5507922c56a529a738795c5ef6b4b9592d9faf6
Adrian McIntosh <adriandonaldmcintosh@gmail.com>
2023-11-19 19:23:42 -0800

    updated SensorData.js

--- final-project/src/SensorData.js
+++ final-project/src/SensorData.js
@@ -1,16 +1,21 @@
 // src/components/SensorData.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import PongGame from './PongGame'; // Import the PongGame component
+
+import Axes from './Axes'; // Import the PongGame component
 
 const SensorData = () => {
-    const [sensorValues, setSensorValues] = useState({});
+    const [sensorValues1, setSensorValues1] = useState({});
+    const [sensorValues2, setSensorValues2] = useState({});
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const response = await axios.post('http://localhost:3000/');
-                setSensorValues(response.data.sensorValues);
+
+                setSensorValues1(response.data.sensorValues1);
+                setSensorValues2(response.data.sensorValues2);
+
             } catch (error) {
                 console.error('Error fetching sensor data:', error);
             }
@@ -34,9 +39,9 @@ const SensorData = () => {
             </ul>
 
             {/* Pass the sensor value for Player 1 as a prop */}
-            <PongGame
-                player1SensorValue={sensorValues["2103"]}
-                player2SensorValue={sensorValues["2102"]} // Add this line
+            <Axes
+                Arduino_Values_A={[sensorValues1["2101"], sensorValues1["2102"], sensorValues1["2103"], sensorValues1["2104"]]}
+                Arduino_Values_B={[sensorValues2["2101"], sensorValues2["2102"], sensorValues2["2103"], sensorValues2["2104"]]}
             />
         </div>
     );
