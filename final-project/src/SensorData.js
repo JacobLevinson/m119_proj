b9a03aeed89782622df3831e58eacea3502be106
Adrian McIntosh <adriandonaldmcintosh@gmail.com>
2023-11-19 20:22:15 -0800

    added some presentation code

--- final-project/src/SensorData.js
+++ final-project/src/SensorData.js
@@ -5,40 +5,68 @@ import axios from 'axios';
 import SensorGrid from './SensorGrid'; // Import the PongGame component
 
 const SensorData = () => {
-    const [sensorValues1, setSensorValues1] = useState([1, 2, 3, 4]);
-    const [sensorValues2, setSensorValues2] = useState({});
+    const [sensorValues1, setSensorValues1] = useState({
+        "2101": 1,
+        "2102": 2,
+        "2103": 3,
+        "2104": 4
+    });
+    const [sensorValues2, setSensorValues2] = useState({
+        "2101": 5,
+        "2102": 6,
+        "2103": 7,
+        "2104": 8
+    });
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.post('http://localhost:3000/');
+        const fetchData = () => {
+            // Simulate changing sensor values
+            setSensorValues1(prevValues => ({
+                "2101": getRandomValue(prevValues["2101"]),
+                "2102": getRandomValue(prevValues["2102"]),
+                "2103": getRandomValue(prevValues["2103"]),
+                "2104": getRandomValue(prevValues["2104"])
+            }));
 
-                setSensorValues1(response.data.sensorValues1);
-                setSensorValues2(response.data.sensorValues2);
-
-            } catch (error) {
-                console.error('Error fetching sensor data:', error);
-            }
+            setSensorValues2(prevValues => ({
+                "2101": getRandomValue(prevValues["2101"]),
+                "2102": getRandomValue(prevValues["2102"]),
+                "2103": getRandomValue(prevValues["2103"]),
+                "2104": getRandomValue(prevValues["2104"])
+            }));
         };
 
+        const getRandomValue = (prevValue) => {
+            const min = prevValue - 1;
+            const max = prevValue + 1;
+            return Math.random() * (max - min) + min;
+        };
+
+
+
+
+
+
+        // const fetchData = async () => {
+        //     try {
+        //         const response = await axios.post('http://localhost:3000/');
+
+        //         setSensorValues1(response.data.sensorValues1);
+        //         setSensorValues2(response.data.sensorValues2);
+
+        //     } catch (error) {
+        //         console.error('Error fetching sensor data:', error);
+        //     }
+        // };
+
         fetchData();
-        const interval = setInterval(fetchData, 100); // Adjust the interval as needed
+        const interval = setInterval(fetchData, 1); // Adjust the interval as needed
 
         return () => clearInterval(interval);
     }, []);
 
     return (
         <div>
-            {/* <h2>Sensor Data:</h2>
-            <ul>
-                {Object.entries(sensorValues1).map(([uuid, value]) => (
-                    <li key={uuid}>
-                        {`Characteristic ${uuid}: ${value}`}
-                    </li>
-                ))}
-            </ul> */}
-
-            {/* Pass the sensor value for Player 1 as a prop */}
             <SensorGrid
                 Arduino_Values_A={[sensorValues1["2101"], sensorValues1["2102"], sensorValues1["2103"], sensorValues1["2104"]]}
                 Arduino_Values_B={[sensorValues2["2101"], sensorValues2["2102"], sensorValues2["2103"], sensorValues2["2104"]]}
--- final-project/src/SensorGrid.js
+++ final-project/src/SensorGrid.js
@@ -1,27 +1,34 @@
 // SensorGrid.js
 
-import React, {useState} from 'react';
-
-
+import React, {useState, useEffect} from 'react';
 
 const SensorGrid = ({ Arduino_Values_A, Arduino_Values_B }) => {
   const [sensorValues1, setSensorValues1] = useState(Arduino_Values_A);
   const [sensorValues2, setSensorValues2] = useState(Arduino_Values_B);
 
+  useEffect(() => {
+    setSensorValues1(Arduino_Values_A);
+    setSensorValues2(Arduino_Values_B);
+  }, [
+        Arduino_Values_A,
+        Arduino_Values_B,
+    ]);
+
+
   return (
     <div className="sensor-grid">
       {/* Render the four objects */}
-      <div className="sensor-object" style={{ left: '20%', bottom: '20%' }}></div>
-      <div className="sensor-object" style={{ left: '40%', bottom: '30%' }}></div>
-      <div className="sensor-object" style={{ left: '60%', bottom: '25%' }}></div>
-      <div className="sensor-object" style={{ left: '80%', bottom: '15%' }}></div>
+      <div className="sensor-object" style={{ left: '20%', bottom: '90%' }}></div>
+      <div className="sensor-object" style={{ left: '40%', bottom: '90%' }}></div>
+      <div className="sensor-object" style={{ left: '60%', bottom: '90%' }}></div>
+      <div className="sensor-object" style={{ left: '80%', bottom: '90%' }}></div>
 
       {/* Render floating dots based on sensor data */}
       {sensorValues1.map((distance, index) => (
         <div
           key={index}
           className="floating-dot"
-          style={{ left: `${index * 20}%`, bottom: `${distance}%` }}
+          style={{ left: `${(index+1) * 20}%`, bottom: `${distance/4}%` }}
         ></div>
       ))}
     </div>
