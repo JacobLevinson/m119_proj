f37b8e6a327ced4c76524a954e2b342563980460
Jacob Levinson <96364327+JacobLevinson@users.noreply.github.com>
2023-11-19 19:01:45 -0800

    Added reading from 2 arduinos at once

--- central.js
+++ central.js
@@ -1,25 +1,22 @@
-// based on the example on https://www.npmjs.com/package/@abandonware/noble
-
-// import Pong from 'react-pong'
-
 const noble = require('@abandonware/noble');
 
-const uuid_service = "1101"
+const uuid_services = ["1101", "1102"]; // Array of service UUIDs
 const uuid_values = ["2101", "2102", "2103"]; // Array of UUIDs
 
-let sensorValues = {};
+let sensorValues1 = {};
+let sensorValues2 = {};
 
 noble.on('stateChange', async (state) => {
     if (state === 'poweredOn') {
         console.log("start scanning");
-        await noble.startScanningAsync([uuid_service], false);
+        await noble.startScanningAsync(uuid_services, false);
     }
 });
 
 noble.on('discover', async (peripheral) => {
     await noble.stopScanningAsync();
     await peripheral.connectAsync();
-    const { characteristics } = await peripheral.discoverSomeServicesAndCharacteristicsAsync([uuid_service], uuid_values);
+    const { characteristics } = await peripheral.discoverSomeServicesAndCharacteristicsAsync(uuid_services, uuid_values);
 
     // Read data for each characteristic
     characteristics.forEach((characteristic) => {
@@ -32,9 +29,16 @@ noble.on('discover', async (peripheral) => {
 //
 let readData = async (characteristic) => {
     const value = (await characteristic.readAsync());
+    const serviceUuid = characteristic._serviceUuid;
     const uuid = characteristic.uuid;
-    sensorValues[uuid] = value.readFloatLE(0);
-    console.log(`Characteristic ${uuid}: ${sensorValues[uuid]}`);
+
+    if (serviceUuid === '1101') {
+        sensorValues1[uuid] = value.readFloatLE(0);
+        console.log(`Characteristic ${uuid} from Service 1101: ${sensorValues1[uuid]}`);
+    } else if (serviceUuid === '1102') {
+        sensorValues2[uuid] = value.readFloatLE(0);
+        console.log(`Characteristic ${uuid} from Service 1102: ${sensorValues2[uuid]}`);
+    }
 
     // read data again in t milliseconds
     setTimeout(() => {
@@ -47,9 +51,15 @@ let readData = async (characteristic) => {
 // based on example code on https://expressjs.com/
 //
 const express = require('express')
-const app = express()
+const cors = require('cors'); // Import the 'cors' middleware
+
+const app = express();
+
+app.use(cors()); // Enable CORS for all routes
 const port = 3000
 
+// Specify the directory where your EJS templates are located (the "views" directory).
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
@@ -61,7 +71,8 @@ app.post('/', (req, res) => {
         'Content-Type': 'application/json'
     });
     res.end(JSON.stringify({
-        sensorValue: sensorValue
+        sensorValues1: sensorValues1,
+        sensorValues2: sensorValues2
     }))
 })
 
